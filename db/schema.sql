072c6f59ae5be00796d179400c932701a014f646
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-02-03 20:45:23 +0000

    schema file modified

--- db/schema.sql
+++ db/schema.sql
@@ -5,6 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	eaten BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
