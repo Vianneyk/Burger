efccfa2143ea2a1deb247a50fb753d0b346fc175
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-02-03 21:19:59 +0000

    connection.js created and connected the node to MySQL

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Marceline1!',
+    database: 'burgers_db',
+});
+
+connection.connect(function (err) {
+    //If there is an error when connecting to the database, log the error to the console.
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    //If a database connection is established, log the database thread number.
+    console.log("connected as id " + connection.threadId);
+});
+
+//Export the connection properties so that we can use them in other files.
+
+module.exports = connection;
+
