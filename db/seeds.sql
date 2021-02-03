4c3227384360f1603aeafe715c811f67951e20fc
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-02-03 20:53:15 +0000

    schema.sql amended and seeds.sql created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Great Burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Deluxe Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grande Burger", true);
