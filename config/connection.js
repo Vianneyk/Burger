6793f8b56fac9df7de80f07b82d0166f695a0781
Vianney Kabamba <vianneyk@hotmail.co.uk>
2021-02-07 20:13:42 +0000

    amended connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+  else{
+
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -7,6 +12,8 @@ var connection = mysql.createConnection({
     password: 'Marceline1!',
     database: 'burgers_db',
 });
+
+  }
 
 connection.connect(function (err) {
     //If there is an error when connecting to the database, log the error to the console.
